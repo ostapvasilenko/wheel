de802b88f25c324ff7f727344286d31fa97c48f1
ostapvasulenko <ostapvasilenko55@gmail.com>
2023-07-18 14:17:47 +0300

    create contacts layout

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,18 @@
 
+function openPage(event, pageName) {
+    var i, tabContent, tabButton;
+    tabContent = document.getElementsByClassName("tab-content");
+    for (i = 0; i < tabContent.length; i++) {
+      tabContent[i].style.display = "none";
+    }
+    tabButton = document.getElementsByClassName("tab-button");
+    for (i = 0; i < tabButton.length; i++) {
+      tabButton[i].className = tabButton[i].className.replace(" active", "");
+    }
+    document.getElementById(pageName).style.display = "block";
+    event.currentTarget.className += " active";
+  }
+  
+  // За замовчуванням відкриємо першу сторінку
+  document.getElementById("page2").style.display = "block";
+  document.getElementsByClassName("tab-button")[0].className += " active";
