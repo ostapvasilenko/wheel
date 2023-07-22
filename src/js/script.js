5c404818cffa08af734e9c75bdbd4ded5686c9fe
ostapvasulenko <ostapvasilenko55@gmail.com>
2023-07-22 17:16:56 +0300

    finish make page reviews and edited the rest of the code

--- src/js/script.js
+++ src/js/script.js
@@ -1,18 +1,18 @@
 
 function openPage(event, pageName) {
-    var i, tabContent, tabButton;
-    tabContent = document.getElementsByClassName("tab-content");
-    for (i = 0; i < tabContent.length; i++) {
-      tabContent[i].style.display = "none";
-    }
-    tabButton = document.getElementsByClassName("tab-button");
-    for (i = 0; i < tabButton.length; i++) {
-      tabButton[i].className = tabButton[i].className.replace(" active", "");
-    }
-    document.getElementById(pageName).style.display = "block";
-    event.currentTarget.className += " active";
+  var i, tabContent, tabButton;
+  tabContent = document.getElementsByClassName("tab-content");
+  for (i = 0; i < tabContent.length; i++) {
+    tabContent[i].style.display = "none";
   }
-  
-  // За замовчуванням відкриємо першу сторінку
-  document.getElementById("page3").style.display = "block";
-  document.getElementsByClassName("tab-button")[0].className += " active";
+  tabButton = document.getElementsByClassName("tab-button");
+  for (i = 0; i < tabButton.length; i++) {
+    tabButton[i].className = tabButton[i].className.replace(" active", "");
+  }
+  document.getElementById(pageName).style.display = "block";
+  event.currentTarget.className += " active";
+}
+
+// За замовчуванням відкриємо першу сторінку
+document.getElementById("page1").style.display = "block";
+document.getElementsByClassName("tab-button")[0].className += " active";
